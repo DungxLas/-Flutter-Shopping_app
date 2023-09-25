9fb1e6e525959855bf0b207eee61e274cfbeac57
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-09-25 21:29:05 +0700

    25/09/2023

--- shopping_app/lib/widgets/new_item.dart
+++ shopping_app/lib/widgets/new_item.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 import 'package:shopping_app/data/categories.dart';
 import 'package:shopping_app/model/category.dart';
@@ -20,14 +24,29 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
-        ),
+      final url = Uri.https(
+        'flutter-prep-7878d-default-rtdb.firebaseio.com',
+        'shopping-list.json',
       );
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title,
+        }),
+      );
+      // Navigator.of(context).pop(
+      //   GroceryItem(
+      //     id: DateTime.now().toString(),
+      //     name: _enteredName,
+      //     quantity: _enteredQuantity,
+      //     category: _selectedCategory,
+      //   ),
+      // );
     }
   }
 
